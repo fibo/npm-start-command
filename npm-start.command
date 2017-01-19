7775039058c55213468613fe38a8deaa5f331847
fibo <casati_gianluca@yahoo.it>
2017-01-19 12:21:20 +0100

    added npm-start

---
+++ npm-start.command
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# make a shell script change to the folder in which it is located
+cd -- "$(dirname "$BASH_SOURCE")"
+
+if [ -e package.json ]; then
+  npm start
+else
+  osascript -e 'tell app "System Events" to display dialog "File package.json not found"'
+  echo
+fi
+
---
+++ postinstall.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const path = require('path')
+const os = require('os')
+
+const platform = os.platform()
+const upFolder = path.resolve(path.join(__dirname, '..', '..'))
+
+// Do nothing if package is installed globally.
+if (require('module').globalPaths.indexOf(upFolder) === -1) {
+  var fileName = null
+
+  if (platform === 'darwin') fileName = 'npm-start.command'
+
+  if (fileName) copyIfItDoesNotExist(fileName)
+}
+
+function copyIfItDoesNotExist (fileName) {
+  const filePath = path.join(__dirname, fileName)
+  const filePathUp = path.join(__dirname, '..', '..', fileName)
+
+  fs.stat(filePath, function (err, pathStat) {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+
+    if (pathStat.isFile()) {
+      fs.stat(filePathUp, function (errUp, pathStatUp) {
+        // If file does not exist, copy it.
+        if (errUp && errUp.code === 'ENOENT') {
+          fs.createReadStream(filePath)
+            .pipe(fs.createWriteStream(filePathUp))
+            .on('error', console.error)
+        }
+      })
+    }
+  })
+}
